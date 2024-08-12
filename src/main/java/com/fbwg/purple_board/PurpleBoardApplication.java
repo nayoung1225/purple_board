9853f7f68535f4ab050f54239b315391ba3a40f7
nayoung1225 <sjo608@naver.com>
2024-08-12 17:56:16 +0900

    initial commit

---
+++ src/main/java/com/fbwg/purple_board/PurpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fbwg.purple_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurpleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fbwg/purple_board/PurpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbwg.purple_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
